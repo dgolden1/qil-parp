0e8fc0dc3785716af5e2c62e5a4f00ae4bd10f87
Daniel Golden <dgolden1@gmail.com>
2014-08-08 08:02:31 -0700

    Add parp dir to path

--- addpaths.m
+++ addpaths.m
@@ -3,7 +3,7 @@ function addpaths
 
 % base_path = '/Users/dgolden/Documents/qil/qil_software/qil';
 base_path = qilsoftwareroot;
-other_paths = {'', 'common_dig', 'dicom_util', 'image_features'};
+other_paths = {'', 'common_dig', 'dicom_util', 'image_features', 'parp'};
 
 for kk = 1:length(other_paths)
   addpath(fullfile(base_path, other_paths{kk}));
