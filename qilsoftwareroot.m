120785ecabe0c7375222f95acfb52211b35799fe
Daniel Golden <dgolden1@gmail.com>
2014-08-08 08:01:22 -0700

    Move qilsoftware root to root directory and remove hard coded path
    By using mfilename, it should now be system independent

---
+++ qilsoftwareroot.m
@@ -0,0 +1,6 @@
+function p = qilsoftwareroot
+% Directory of QIL software
+
+% By Daniel Golden (dgolden1 at gmail dot com) May 2014
+this_script_filename = mfilename('fullpath');
+p = fileparts(this_script_filename);
