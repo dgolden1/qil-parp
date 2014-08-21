486a87f0495f21e317f0248e5a668dd73d52b01b
Daniel Golden <dgolden1@gmail.com>
2014-08-21 08:43:00 -0700

    Remove startup.m

--- common_dig/startup.m
+++
@@ -1,4 +0,0 @@
-% Matlab 2012a startup file for goldenmac
-% By Daniel Golden (dgolden1 at stanford dot edu) May 2012
-
-set(0, 'defaultfigureposition', [1700 700 560 420]); % Good for my giant 2560x1440 27" iMac monitor
