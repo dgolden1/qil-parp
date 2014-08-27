62c3f084f411bbc748f589a5501fb93d511acd32
Daniel Golden <dgolden1@gmail.com>
2014-08-27 07:53:32 -0700

    Remove non-ail directories from path on dantop

--- addpaths.m
+++ addpaths.m
@@ -8,3 +8,13 @@ other_paths = {'', 'common_dig', 'dicom_util', 'image_features', 'parp'};
 for kk = 1:length(other_paths)
   addpath(fullfile(base_path, other_paths{kk}));
 end
+
+% Remove paths outside qil directory
+if isdantop
+  p = path;
+  p_cell = strsplit(p, ':');
+  paths_to_remove = p_cell(~cellfun(@isempty, regexp(p_cell, '^/Users/dgolden/software', 'once')));
+  for kk = 1:length(paths_to_remove)
+    rmpath(paths_to_remove{kk});
+  end
+end
